3ac1c482beb8c2195c18ecc044ff9c1b988afa7f
lailatulfitriq <lfitri1012@gmail.com>
2021-04-12 22:24:12 +0700

    Praktikum WEB Minggu 9

---
+++ Minggu8/ci/application/views/helloview2.php
@@ -0,0 +1,10 @@
+<html>
+	<!--<head>Controller dan View</head>-->
+	<head>
+		<title>Controller, Model dan View</title>
+	</head>
+	<body>
+		<h2><? echo $teks; ?></h2>
+		<h3>Menggunakan Controller, Model dan View</h3>
+	</body>
+</html>
